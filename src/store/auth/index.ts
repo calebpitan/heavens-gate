897f1be88d434980ac3c5fb559a32ebb81b6b1a3
Caleb Adepitan <calebpitan@outlook.com>
2021-01-26 04:24:30 +0100

    Extend and refactor actions

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,28 +1,33 @@
 import { Module } from 'vuex';
 import { heavensGate } from '@/api';
 import { RootState } from '@/store/types';
-import { login, refreshToken, signup } from '@/services/auth';
+import { getAuthStatus, login, refreshToken, signup } from '@/services/auth';
 import { AuthMutationType, AuthActionType, SignupActionPayload, LoginActionPayload } from './types';
 
-export type AuthStore = { data: { id: string; token: string; loggedIn: boolean } };
+export type AuthState = { data: { id: string; token: string; loggedIn: boolean } };
 export type AuthStoreModule = typeof authStore;
 
-const authStore: Module<AuthStore, RootState> = {
+const authStore: Module<AuthState, RootState> = {
   state() {
     return { data: { id: '', token: '', loggedIn: false } };
   },
 
   mutations: {
-    [AuthMutationType.SET_TOKEN](state, payload: AuthStore['data']) {
+    [AuthMutationType.SET_TOKEN](state, payload: AuthState['data']) {
       state.data = { ...state.data, ...payload, loggedIn: true };
     },
 
-    [AuthMutationType.SET_LOGGED_IN](state, payload: Pick<AuthStore['data'], 'loggedIn'>) {
+    [AuthMutationType.SET_LOGGED_IN](state, payload: Pick<AuthState['data'], 'loggedIn'>) {
       state.data.loggedIn = payload.loggedIn;
     },
   },
 
   actions: {
+    async [AuthActionType.CHECK_LOGGED_IN](context) {
+      const { data } = await getAuthStatus();
+      context.commit(AuthMutationType.SET_LOGGED_IN, { loggedIn: data.session });
+    },
+
     async [AuthActionType.REFRESH_TOKEN](context) {
       const { data } = await refreshToken(heavensGate);
       context.commit(AuthMutationType.SET_TOKEN, data);
@@ -34,7 +39,7 @@ const authStore: Module<AuthStore, RootState> = {
     },
 
     async [AuthActionType.LOGIN](context, { payload }: LoginActionPayload) {
-      const { data } = await login(payload);
+      const { data } = await login(payload!);
       context.commit(AuthMutationType.SET_TOKEN, data);
     },
   },
