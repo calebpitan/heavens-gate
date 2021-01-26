630fb6a8140a7785fe118e994a4f89954004f6e3
Caleb Adepitan <calebpitan@outlook.com>
2021-01-26 08:22:11 +0100

    Make guarded (authorized) routes exclusive
    * Specify whether a route is/should be guarded in its declaration `meta`
    attribute
    
    * On application initial mount check if user's is logged in (authenticated)
    
    * Watch for route changes and run a guard function
    
    * The guard function, if the route is defined as a guarded one, redirects
    to a login page when user isn't authenticated. If the route is one of
    'login' or 'signup' and user is authenticated, it redirects to home.
    If none of these evaluates to true, it stays on the current route.
    
    * Checking for the user's auth status only takes place on initial
    application startup, while the guard runs on every route change.

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,56 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, watch } from 'vue';
+import { useStore } from 'vuex';
+import { AuthActionType, CheckLoggedInActionPayload } from './store/auth/types';
+import { RootState } from './store/types';
 
 export default defineComponent({
   name: 'App',
+  // watch: { $route: 'guard' },
+  data() {
+    return { hasCheckedAuthStatus: false };
+  },
+  setup() {
+    const store = useStore<RootState>();
+    return { loggedIn: computed(() => store.state.auth.data.loggedIn) };
+  },
+  methods: {
+    guard() {
+      const route = this.$route;
+
+      if (route.matched.length === 0 || !this.$data.hasCheckedAuthStatus) return;
+
+      const loggedIn = this.loggedIn;
+      const guarded = route.meta.guarded as boolean;
+      const unguards = ['Login', 'Signup'];
+
+      if (guarded && !loggedIn) {
+        this.$router.push('/login');
+      } else if (unguards.includes(route.name as string) && loggedIn) {
+        this.$router.push('/');
+      }
+    },
+  },
+  mounted() {
+    watch(
+      () => this.$route,
+      () => this.guard(),
+    );
+    this.$store
+      .dispatch<CheckLoggedInActionPayload>({ type: AuthActionType.CHECK_LOGGED_IN })
+      .then(() => (this.$data.hasCheckedAuthStatus = true))
+      .then(() => this.guard());
+  },
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
-  /* margin-top: 60px; */
 }
 </style>
