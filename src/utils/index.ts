f8855224e18f90bb4240d508555f4eebd59ba970
Caleb Adepitan <calebpitan@outlook.com>
2021-01-28 14:31:24 +0100

    Add more utility functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { AuthState } from '@/store/auth';
 
-export const getAuth = () => {
+export const getAuthFromStorage = () => {
   try {
     const auth = window.localStorage.getItem('auth');
     return auth ? `Bearer ${auth}` : auth;
@@ -15,3 +15,20 @@ export const getAuthFromStore = (store: AuthState) => {
   const token = store.data.token;
   return token.length > 0 ? token : undefined;
 };
+
+export const deepCopy = (o: Record<string, any>) => {
+  return JSON.parse(JSON.stringify(o));
+};
+
+export function copyIntoObject<T extends Record<string, any>>(from: T, toCtr: new () => T): T {
+  const to = new toCtr();
+  const data: T = deepCopy(from);
+  const keys: Array<keyof T> = Object.keys(from);
+  keys.forEach(key => (to[key] = data[key]));
+  return to;
+}
+
+export function resetValidation<T extends Record<string, boolean>>(validation: T) {
+  const keys = Object.keys(validation) as Array<keyof T>;
+  keys.forEach(key => (validation[key] = true as T[typeof key]));
+}
