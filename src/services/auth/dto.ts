436eba2604ddbc70303b22b3b2664dcb1ecc0e5b
Caleb Adepitan <calebpitan@outlook.com>
2021-01-28 14:23:55 +0100

    Add validation decorators to DTO schema

--- src/services/auth/dto.ts
+++ src/services/auth/dto.ts
@@ -1,21 +1,51 @@
+import {
+  IsEmail,
+  IsString,
+  IsNotEmptyObject,
+  IsNotEmpty,
+  IsUUID,
+  Length,
+  ValidateNested,
+  IsOptional,
+} from 'class-validator';
+
 export class Identifier {
+  @IsOptional()
+  @IsNotEmpty()
   username?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
   email?: string;
 }
 
 export class CredentialDto {
+  @IsNotEmptyObject()
+  @ValidateNested()
   identifier!: Identifier;
+
+  @IsNotEmpty()
   password!: string;
 }
 
 export class CreateUserDto {
+  @Length(1, 32)
   firstname!: string;
+
+  @Length(1, 32)
   lastname!: string;
+
+  @Length(3, 24)
   username!: string;
-  email!: string;
+
+  @Length(8)
   password!: string;
+
+  @IsEmail(undefined, { message: "You haven't entered a valid $property address" })
+  email!: string;
 }
 
 export class SessionDto {
+  @IsUUID()
   sid!: string;
 }
