f36159b4b9a8a005b52a55266743ea0f84c02fd7
Caleb Adepitan <calebpitan@outlook.com>
2021-01-29 18:33:25 +0100

    Add input Submit button

---
+++ src/components/input/Submit.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseInput
+    v-model="value"
+    ref="_submit"
+    type="submit"
+    class="bg-black text-white font-semibold border-black shadow-2xl focus:ring-black hover:opacity-90 transition-all duration-150"
+  />
+</template>
+
+<script lang="ts">
+import BaseInput from './BaseInput.vue';
+
+export default {
+  name: 'Submit',
+  components: { BaseInput },
+  data() {
+    return { value: this.$attrs.value || '' };
+  },
+};
+</script>
+
+<style lang="postcss" scoped></style>
