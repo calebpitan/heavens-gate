2a5602cd05112335fa84008a5e3500f468b14898
Caleb Adepitan <calebpitan@outlook.com>
2021-01-29 18:30:39 +0100

    Rename, organize files and restructure markup
    * Organize components into folders of responsibility
    
    * Restructure markup to present a better layout, visual representation of
    components and functionality

--- src/components/input/Checkbox.vue
+++ src/components/input/Checkbox.vue
@@ -9,17 +9,23 @@
     class="checkbox sr-only"
   />
   <span
-    :class="`w-4 h-4 border-2 ${disabled ? 'border-gray-200' : 'border-gray-400'} rounded-sm transition-colors
+    :class="`flex items-center justify-center w-4 h-4 border-2 ${
+      disabled ? 'border-gray-200' : 'border-gray-500'
+    } rounded-sm transition-colors
   transition-shadow duration-500`"
     @click="toggle"
-  />
+  >
+    <Icon v-if="checkbox.checked" name="check" stroke-width="3" class="pointer-events-none" />
+  </span>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue';
+import Icon from '../Icon.vue';
 
 export default {
   name: 'Checkbox',
+  components: { Icon },
   inheritAttrs: false,
   props: { checked: Boolean, disabled: Boolean },
   data() {
@@ -38,17 +44,18 @@ export default {
 };
 </script>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
 .checkbox {
   /* visibility: hidden; */
 }
+
 .checkbox:focus:not(:checked) + span {
   box-shadow: 0 0 0 0.05rem black;
 }
+
 .checkbox:checked + span {
-  @apply w-3.5 h-3.5;
+  @apply w-3.5 h-3.5 p-2;
   border: 0;
   background: black;
-  box-shadow: 0 0 0 0.09rem white, 0 0 0 0.18rem black;
 }
 </style>
---
+++ src/components/input/Input.vue
@@ -0,0 +1,15 @@
+<template>
+  <BaseInput ref="input" v-model="value" class="border-gray-300 focus:ring-black" />
+</template>
+
+<script lang="ts">
+import BaseInput from './BaseInput.vue';
+
+export default {
+  name: 'Input',
+  components: { BaseInput },
+  data() {
+    return { value: this.$attrs.value || '' };
+  },
+};
+</script>
