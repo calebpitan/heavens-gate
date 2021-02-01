9c7acf2fae9daae14027ec94fc81dc09a215d4f3
Caleb Adepitan <calebpitan@outlook.com>
2021-02-01 14:06:25 +0100

    Create default app link component

---
+++ src/components/AppLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link class="text-pink-800 dark:text-pink-500 hover:text-current transition-color duration-500">
+    {{ text }}<slot />
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'AppLink',
+  props: {
+    text: { type: String, default: null },
+  },
+};
+</script>
+
+<style lang="postcss" scoped></style>
