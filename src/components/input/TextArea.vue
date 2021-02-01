ff399794b4022759a865fa7260431860c1c927a9
Caleb Adepitan <calebpitan@outlook.com>
2021-02-01 14:12:24 +0100

    Add textarea component

---
+++ src/components/input/TextArea.vue
@@ -0,0 +1,18 @@
+<template>
+  <textarea
+    ref="textarea"
+    v-model="value"
+    class="w-full rounded-md px-4 py-2 md:py-3 border-1 focus:outline-none focus:border-transparent focus:ring-2 transition-shadow duration-500"
+  />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BaseInput',
+  data() {
+    return { value: this.$attrs.value || '' };
+  },
+};
+</script>
+
+<style scoped></style>
