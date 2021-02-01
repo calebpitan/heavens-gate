410e576194868966349e10663caf58879b604e53
Caleb Adepitan <calebpitan@outlook.com>
2021-02-01 14:05:35 +0100

    Create a general application theme

---
+++ src/components/AppTheme.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-full bg-gray-50, dark:bg-gray-800 text-gray-500 dark:text-gray-300">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTheme',
+  data() {
+    const theme = 'bg-gray-50 dark:bg-gray-900 text-gray-500 dark:text-gray-300'.split(' ');
+    return { theme };
+  },
+
+  mounted() {
+    document.documentElement.classList.add(...this.$data.theme);
+  },
+
+  unmounted() {
+    document.documentElement.classList.remove(...this.$data.theme);
+  },
+};
+</script>
+
+<style lang="postcss" scoped></style>
