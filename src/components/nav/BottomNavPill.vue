2dc5d3da062b96b116e64376c71763e8f3bc4e45
Caleb Adepitan <calebpitan@outlook.com>
2021-02-01 14:09:50 +0100

    Add bottom navigation

---
+++ src/components/nav/BottomNavPill.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="inline-flex justify-center items-center text-xs font-semibold">
+    <router-link :to="navTo" class="flex flex-col my-auto items-center text-center px-4 py-3">
+      <Icon :name="icon" class="text-2xl" />
+      {{ itemName }}
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Icon from '../Icon.vue';
+
+export default {
+  components: { Icon },
+  name: 'BottomNavPill',
+  props: {
+    icon: { type: String, default: null },
+    itemName: { type: String, default: 'Item' },
+    navTo: { type: String, default: '#' },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.router-link-exact-active {
+  @apply text-pink-800 dark:text-pink-500;
+}
+</style>
---
+++ src/components/nav/BottomNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="fixed bottom-0 lg:bottom-6 flex mt-auto bg-white dark:bg-gray-700 w-full lg:w-auto lg:rounded-full lg:self-center lg:shadow-2xl lg:space-x-4"
+  >
+    <BottomNavPill class="nav-pill" icon="home" itemName="Home" navTo="/" />
+    <BottomNavPill class="nav-pill" icon="search" itemName="Search" navTo="/search" />
+    <BottomNavPill class="nav-pill" icon="plus" itemName="New" navTo="/new" />
+    <BottomNavPill class="nav-pill" icon="shopping-cart" itemName="Cart" navTo="/cart" />
+    <BottomNavPill class="nav-pill" icon="user" itemName="Profile" navTo="/user" />
+  </div>
+</template>
+
+<script lang="ts">
+import BottomNavPill from './BottomNavPill.vue';
+
+export default {
+  components: { BottomNavPill },
+  name: 'BottomNavigation',
+};
+</script>
+
+<style lang="postcss" scoped>
+.nav-pill {
+  @apply mx-auto;
+}
+.nav-pill:first-of-type {
+  @apply lg:rounded-l-full;
+}
+
+.nav-pill:last-of-type {
+  @apply lg:rounded-r-full;
+}
+</style>
