acd8f455d8f37a6f79db8287265e60d74a1c70b7
Caleb Adepitan <calebpitan@outlook.com>
2021-02-01 14:07:08 +0100

    Make app header appear cooler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,36 +1,50 @@
 <template>
-  <div class="p-4">
-    <div class="header bg-white rounded-md">
-      <header class="flex w100">
-        <div class="flex flex-grow">
-          <router-link
-            to="/"
-            class="px-4 py-2 text-black font-semibold rounded-l-md hover:text-white hover:bg-black transition-all duration-500"
-          >
-            Home
-          </router-link>
-        </div>
-        <div class="flex">
-          <router-link
-            to="/login"
-            class="px-4 py-2 text-black font-semibold hover:text-white hover:bg-black transition-all duration-500"
-          >
-            Login
-          </router-link>
-          <router-link
-            to="/signup"
-            class="px-4 py-2 text-black font-semibold rounded-r-md hover:text-white hover:bg-black transition-all duration-500"
-          >
-            Signup
-          </router-link>
-        </div>
-      </header>
-    </div>
+  <div class="header">
+    <header class="flex items-center">
+      <div class="flex-grow">
+        <button
+          :class="`flex items-center ${
+            canNavigate ? '-ml-2' : ''
+          } text-black dark:text-white text-3xl font-bold focus:outline-none`"
+          @click="navigate"
+        >
+          <Icon v-if="canNavigate" name="chevron-left" strokeWidth="3" />
+          <h1 class="tracking-tight">{{ title }}</h1>
+        </button>
+      </div>
+      <div v-if="loggedIn">
+        <app-link to="/orders" class="flex flex-col items-center text-2xl font-semibold">
+          <Icon name="truck" />
+          <span class="text-xs"> Orders </span>
+        </app-link>
+      </div>
+      <div v-else class="flex items-center space-x-4">
+        <app-link to="/login"> Log in </app-link>
+        <app-link to="/signup"> Sign up </app-link>
+      </div>
+    </header>
   </div>
 </template>
 
 <script lang="ts">
+import AppLink from './AppLink.vue';
+import Icon from './Icon.vue';
+
 export default {
+  components: { Icon, AppLink },
   name: 'Header',
+  props: {
+    title: { type: String, default: 'Home' },
+    loggedIn: { type: Boolean, default: false },
+    canNavigate: { type: Boolean, default: false },
+  },
+  methods: {
+    navigate() {
+      if (this.$props.canNavigate) {
+        return this.$router.history.go(-1);
+      }
+      this.$router.push('/');
+    },
+  },
 };
 </script>
